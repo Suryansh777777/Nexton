5dc85814647eb6d01d2da3c5270383816dfebdcf
Suryansh777777 <suryanshchourasia12@gmail.com>
2024-04-16 18:04:06 +0530

    Navbar Done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from "react";
 import logo from "../assets/Icon.png";
+import { Link } from "react-router-dom";
+import { FaXmark, FaBars } from "react-icons/fa6";
+
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [isSticky, setisSticky] = useState(false);
@@ -32,11 +35,65 @@ const Navbar = () => {
 
   return (
     <header className="w-full bg-white md:bg-transparent fixed top-0 left-0 right-0">
-      <nav>
-        <div>
-          <a href="#home" src={logo}>
-            Nexcent
+      <nav
+        className={`py-4 lg:px-14 px-4${
+          isSticky
+            ? "sticky top-0 left-0 right-0 border-b bg-white duration-300"
+            : ""
+        }`}
+      >
+        <div className="flex justify-between items-center text-base gap-8">
+          <a
+            href=""
+            className="text-2xl font-semibold flex items-center space-3"
+          >
+            <img src={logo} alt="" className="w-10 inline-block items-center" />
+            <span className="text-[#263238]">NEXCENT</span>
           </a>
+
+          {/* large device menu */}
+
+          <ul className="hidden md:flex space-x-12">
+            {navItems.map(({ link, path }) => (
+              <Link
+                to={path}
+                spy={true}
+                smooth={true}
+                offset={-100}
+                key={path}
+                className="block text-base text-gray900 hover:text-brandPrimary first:font-medium"
+              >
+                {link}
+              </Link>
+            ))}
+          </ul>
+          {/* button for large */}
+          <div className="space-x-12 hidden lg:flex items-center">
+            <a
+              href=""
+              className="hidden lg:flex items-center text-brandPrimary hover:text-gray900"
+            >
+              Login
+            </a>
+            <button className="bg-brandPrimary text-white py-2 px-4 transition-all duration-300 rounded hover:bg-neutralGrey">
+              Contact us
+            </button>
+          </div>
+
+          {/* menu button for small */}
+
+          <div className="md:hidden">
+            <button
+              onClick={toggleMenu}
+              className="text-neutralDGrey focus:outline-none focus:text-gray-500"
+            >
+              {isMenuOpen ? (
+                <FaXmark className="h-6 w-6 " />
+              ) : (
+                <FaBars className="h-6 w-6 " />
+              )}
+            </button>
+          </div>
         </div>
       </nav>
     </header>
